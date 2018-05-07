903d6520e7f844bbd4e074cc8ffded41acdc9d71
e12exp <e12exp@localhost.localdomain>
2018-05-07 12:59:39 +0200

    stuff

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,26 +12,28 @@ if [[ "$#" -lt "1" ]]; then
 	exit -1
 fi
 
+export LC_ALL=C # shut up, perl
+
 HOSTNAME="$1"
 WDABS="$PWD"
 WD=$(echo $PWD | sed -e "s#$HOME##g" | sed -e "s#^/##g")
 
 # Check if subdirectories are populated
-if [[ -z $(ls mbs/) || -z $(ls ucesb/) || -z $(ls febex_set_param/) ]]; then
+#if [[ -z $(ls mbs/) || -z $(ls ucesb/) || -z $(ls febex_set_param/) ]]; then
 	# Not the case -> check out via git
 #	git submodule init || exit $?
 	git submodule update --init --recursive || exit $?
-fi
+#fi
 
 # Check if we need to build setpar
-if [[ ! -f febex_set_param/setpar ]]; then
-	cd febex_set_param
-	make || exit $?
-	cd ../mbs
-	rm -f setpar
-	ln -s $WDABS/febex_set_param/setpar ./setpar
-	cd ..
-fi
+#if [[ ! -f febex_set_param/setpar ]]; then
+#	cd febex_set_param
+#	make || exit $?
+#	cd ../mbs
+#	rm -f setpar
+#	ln -s $WDABS/febex_set_param/setpar ./setpar
+#	cd ..
+#fi
 
 # Check if we need to build ucesb empty
 if [[ ! -f ucesb/empty/empty ]]; then
