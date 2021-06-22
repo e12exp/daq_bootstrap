e70dac64850539caf80db63cebb6bf6215d58ba1
Haik Simon <land@lx-pool.gsi.de>
2021-06-22 16:45:46 +0200

    fixed rate limiter eof detection

--- scripts/rate-limit.py
+++ scripts/rate-limit.py
@@ -1,30 +1,39 @@
 #!/usr/bin/python
 
 import sys
+
 from time import time, sleep
 from  fcntl import *
 from termios import FIONREAD
 import array
+import ctypes
+libc=ctypes.CDLL("libc.so.6")
+read=libc.read
+
 limit=100
 lastt=time()
 count=0
 
+blen=200
+c=array.array('I', [0])
 
-    
+stdin=sys.stdin.fileno()
+
 while 1:
     if time()>=1+lastt:
         count=0
         lastt=time()
-    c=array.array('I', [0])
+
     if ioctl(sys.stdin.fileno(), FIONREAD, c)==-1:
         raise RuntimeError("ioctl failed")
-    if c[0]==0:
-        sleep(0.1)
-        continue
+    c[0]=max(c[0], 1)
+
     x=sys.stdin.read(c[0])
-    if not len(x): # EOF
+    if x=="":
+        print("\nread failed, assuming EOF\n")
         exit(0)
     lines=x.split("\n")
+    count-=1
     llen=len(lines)
     for i,l in enumerate(lines):
         count+=1
