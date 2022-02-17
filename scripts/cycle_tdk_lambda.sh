158152069003fe42ff6b4ff9abff297282915339
Haik Simon <land@gsi.de>
2022-02-17 17:11:26 +0100

    tdk script

--- scripts/cycle_tdk_lambda.sh
+++ scripts/cycle_tdk_lambda.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-. $(dirname $0)/config/local_settings.sh
+. $(dirname $0)/../config/local_settings.sh
 test -n "$TDK" && ssh lxg1290 ". /u/land/opt/epics/epicsfind.bash ; caput -c $TDK 0 | grep -v New; sleep 5; caput $TDK 1 | grep -v New ; sleep 1; caget $TDK "
