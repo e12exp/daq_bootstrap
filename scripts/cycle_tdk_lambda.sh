71bd70f40788c6249e669f6ab9738aae6505e098
A land user who did not set git config --local <land@example.com>
2022-06-13 15:34:45 +0200

    more flushing

--- scripts/cycle_tdk_lambda.sh
+++ scripts/cycle_tdk_lambda.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 . $(dirname $0)/../config/local_settings.sh
-test -n "$TDK" && ssh lxg1290 ". /u/land/opt/epics/epicsfind.bash ; caput -c $TDK 0 | grep -v New; sleep 5; caput $TDK 1 | grep -v New ; sleep 1; caget $TDK "
+. /u/land/opt/epics/epicsfind.bash
+export EPICS_CA_ADDR_LIST=landgw01
+
+if test -z "$TDK"
+then
+    echo "No TDK variable!"
+    exit 1
+fi
+caput -c $TDK 0 | grep -v New
+sleep 5
+caput $TDK 1 | grep -v New
+sleep 1
+caget $TDK
